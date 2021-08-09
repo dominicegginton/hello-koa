5ff773f81e3344c2d93fe581ca4e196b52a4ab63
Dominic Egginton <dominic.egginton@gmail.com>
2021-08-09 02:06:41 +0100

    create koa server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello Koa')
+/* IMPORT MODULES */
+import Koa from 'koa'
+
+/* SETUP KOA */
+const app = new Koa()
+
+/* START KOA SERVER */
+app.listen(3000, () => console.log('> server started on port 3000'))
