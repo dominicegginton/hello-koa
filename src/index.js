5999cbdd5393aced4da7de9967896332a4c0cbc0
Dominic Egginton <dominic.egginton@gmail.com>
2021-08-09 02:09:02 +0100

    add port config

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import Koa from 'koa'
 /* SETUP KOA */
 const app = new Koa()
 
+/* SERVER CONFIGURATION */
+const PORT = process.env.PORT || 3000
+
 /* START KOA SERVER */
-app.listen(3000, () => console.log('> server started on port 3000'))
+app.listen(PORT, () => console.log(`> server started on port ${PORT}`))
